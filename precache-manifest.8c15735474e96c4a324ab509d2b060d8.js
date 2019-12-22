2ff0f8fa7149d8ecb72e7d25eb0955977433bedf
Klif4 <klif.cheloul@gmail.com>
2019-12-22 02:34:37 +0100

    Updates

---
+++ precache-manifest.8c15735474e96c4a324ab509d2b060d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e227d709b714539ddcb47b6798b7f1",
+    "url": "/workit_test_maudlemao/index.html"
+  },
+  {
+    "revision": "2b3e7e73fbd9dd145913",
+    "url": "/workit_test_maudlemao/static/css/main.6ad5fb24.chunk.css"
+  },
+  {
+    "revision": "74bafa4c092a0cacf334",
+    "url": "/workit_test_maudlemao/static/js/2.3d096e93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/workit_test_maudlemao/static/js/2.3d096e93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b3e7e73fbd9dd145913",
+    "url": "/workit_test_maudlemao/static/js/main.aaf8111d.chunk.js"
+  },
+  {
+    "revision": "780d8403873d80133a4b",
+    "url": "/workit_test_maudlemao/static/js/runtime-main.1d2b95ce.js"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/workit_test_maudlemao/static/media/react.9a28da9f.svg"
+  }
+]);
